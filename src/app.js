194a8102b508745050ff75f065f04afee9c458fd
Ido Zairi <idozri@gmail.com>
2020-05-19 09:53:34 +0300

    Init commit

---
+++ src/app.js
@@ -0,0 +1,100 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
+// Setuphandlebars engine views locations
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Ido Zairi'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Ido Zairi'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Ido Zairi'
+    })
+})
+
+app.get('/weather', ({ query }, res) => {
+    if (!query.address) {
+        return res.send({
+            error: 'Unable to find location. Try another search'
+        })
+    }
+
+    geocode(query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forcastData) => {
+            if (error) {
+                res.send({ error })
+            } else {
+                res.send({
+                    forecast: forcastData,
+                    location,
+                    address: query.address
+                })
+            }
+        })
+    })
+})
+
+app.get('/products', ({ query }, res) => {
+    if (!query.search) {
+        return res.send({
+            error: 'unable to find location. Try another search'
+        })
+    }
+
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'Help',
+        name: 'Ido Zairi',
+        error: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Help',
+        name: 'Ido Zairi',
+        error: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server id up on port 3000.')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=6d440d8149c43aa5c9cfa93f5d68111a&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location!', undefined)
+        } else {
+            const weatherDesc = body.current.weather_descriptions[0]
+            const temperature = body.current.temperature
+            const feelsLike = body.current.feelslike
+
+            callback(undefined, weatherDesc + ". It is currently " + temperature + " degress out. It feels like " + feelsLike + " degress out.")
+        }
+    })
+}
+
+module.exports = forecast
